904898858b293840925f899aff12cc06fd8904a3
79996 <79996@LAPTOP-V6DLTBUR>
2023-08-17 13:07:07 +0300

    Add project files.

---
+++ ProgectForGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgectForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
